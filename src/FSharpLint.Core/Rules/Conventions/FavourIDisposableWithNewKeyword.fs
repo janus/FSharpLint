5665d764ee9655b8585d6d072aac3f342965258c
ijanus <emekamicro@gmail.com>
2022-04-12 14:09:19 +0100

    WIP

--- src/FSharpLint.Core/Rules/Conventions/FavourIDisposableWithNewKeyword.fs
+++ src/FSharpLint.Core/Rules/Conventions/FavourIDisposableWithNewKeyword.fs
@@ -5,14 +5,20 @@ open System.Linq
 open FSharp.Compiler.Text
 open FSharpLint.Framework
 open FSharpLint.Framework.Suggestion
+open FSharp.Compiler.Symbols
+open FSharp.Compiler.Syntax
+open FSharp.Compiler.CodeAnalysis
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
-open FSharp.Compiler.CodeAnalysis
-open FSharp.Compiler.Symbols
 
 
 let private runner (args:AstNodeRuleParams) =
-    Array.empty
+    match (args.AstNode, args.CheckInfo) with
+    | AstNode.Expression(SynExpr.New(_, synType, expression, range)), Some checkInfo ->
+        // Use checkInfo to infer is this new expression implemented IDisposable
+        Array.empty
+
+    | _ -> Array.empty
 
 let rule =
     { Name = "FavourIDisposableWithNewKeyword"

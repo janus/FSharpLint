0a27f5476ff166dc2f8add859884791e81121466
ijanus <emekamicro@gmail.com>
2022-04-19 12:33:09 +0100

    WIP

--- src/FSharpLint.Core/Rules/Conventions/PreferStringInterpolationWithSprintf.fs
+++ src/FSharpLint.Core/Rules/Conventions/PreferStringInterpolationWithSprintf.fs
@@ -8,7 +8,8 @@ open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 open System
 
-let mutable identifiers = Set.empty
+let mutable moduleIdentifiers = Set.empty
+let mutable letIdentifiers = Set.empty
 
 let private isStringFormat (identifiers: List<Ident>) =
     "String" = identifiers.[0].idText && "Format" = identifiers.[1].idText
@@ -16,6 +17,8 @@ let private isStringFormat (identifiers: List<Ident>) =
 let runner args =
     match args.AstNode with
     | AstNode.Expression(SynExpr.App(_, _, SynExpr.LongIdent(_, LongIdentWithDots(ids, _), _, _), paren, range)) when ids.Length = 2 && isStringFormat ids ->
+        let isMember (text: string) =
+            moduleIdentifiers.Contains text || letIdentifiers.Contains text
         match paren with
         | SynExpr.Paren(SynExpr.Tuple(_, [SynExpr.Const(SynConst.String(_, _, _), _); _], _, _), _, _, _) ->
             { Range = range
@@ -23,7 +26,7 @@ let runner args =
               SuggestedFix = None
               TypeChecks = List.empty }
             |> Array.singleton
-        | SynExpr.Paren(SynExpr.Tuple(_, [SynExpr.Ident identifier; _], _, _), _, _, range) when identifiers.Contains identifier.idText ->
+        | SynExpr.Paren(SynExpr.Tuple(_, [SynExpr.Ident identifier; _], _, _), _, _, range) when isMember identifier.idText ->
             { Range = range
               Message = Resources.GetString "RulesPreferStringInterpolationWithSprintf"
               SuggestedFix = None
@@ -31,14 +34,25 @@ let runner args =
             |> Array.singleton
         | _ -> Array.empty
     | AstNode.Binding(SynBinding(_, _, _, _, _, _, _, SynPat.Named(_, identifier, _, _, _), _, SynExpr.Const(SynConst.String(value, _, _), _), range, _)) when value.Contains "{0}" ->
-        identifiers <- identifiers.Add(identifier.idText)
+        let parents = args.GetParents 1
+        match parents with
+        | AstNode.ModuleDeclaration (SynModuleDecl.Let _) :: _ ->
+            moduleIdentifiers <- moduleIdentifiers.Add(identifier.idText)
+        | _ -> letIdentifiers <- letIdentifiers.Add(identifier.idText)
+
+        Array.empty
+    | AstNode.ModuleDeclaration (SynModuleDecl.Let _)   ->
+        letIdentifiers <- Set.empty
         Array.empty
     | AstNode.ModuleDeclaration (SynModuleDecl.NestedModule _)  ->
-        identifiers <- Set.empty
+        moduleIdentifiers <- Set.empty
+        letIdentifiers <- Set.empty
         Array.empty
     | _ -> Array.empty
 
-let cleanup () = identifiers <- Set.empty
+let cleanup () =
+    moduleIdentifiers <- Set.empty
+    letIdentifiers <- Set.empty
 
 let rule =
     { Name = "PreferStringInterpolationWithSprintf"

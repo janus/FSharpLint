bd035be78ad96b6f77462414edb54b99caf4cc5e
ijanus <emekamicro@gmail.com>
2022-04-25 11:59:57 +0100

    CanBeReplacedWithComposition: fix some false positive
    Also, add unit test.
    
    Fixes: https://github.com/fsprojects/FSharpLint/issues/484
    
    TODO: Add extra parameter(boolean) to checkLambda so
    that caller could disable or enable this fix.
    This is becuase two different Rules are calling checkLambda.

--- src/FSharpLint.Core/Rules/Conventions/FunctionReimplementation/FunctionReimplementationHelper.fs
+++ src/FSharpLint.Core/Rules/Conventions/FunctionReimplementation/FunctionReimplementationHelper.fs
@@ -17,13 +17,38 @@ let rec getLambdaParamIdent = function
     | SynSimplePats.Typed(simplePatterns, _, _) -> 
         getLambdaParamIdent simplePatterns
 
+let private isForActivePatten args =
+    let isTargetBinding bindings =
+        let rec loop synBindings =
+            match synBindings with
+            | SynBinding(_, _, _, _, _, _, _, SynPat.LongIdent(LongIdentWithDots(identifiers, _), _, _, _args, _, _), _, _, _, _) :: _ ->
+                identifiers.[0].idText.StartsWith "|" && identifiers.[0].idText.EndsWith "|"
+            | _  ->  false
+        loop bindings
+
+    let rec loop index =
+        let parents = List.rev (args.GetParents index)
+        match parents with
+        | AstNode.ModuleDeclaration(SynModuleDecl.Let (_, bindings, _)) :: _rest ->
+            isTargetBinding bindings
+        | AstNode.ModuleOrNamespace(_) :: rest ->
+            match rest with
+            | AstNode.ModuleDeclaration(SynModuleDecl.Let (_, bindings, _)) :: _rest ->
+                isTargetBinding bindings
+            | _ -> false
+        | _ -> loop (index + 1)
+    loop 1
+
 let checkLambda (args:AstNodeRuleParams) checker =
     match args.AstNode with
-    | AstNode.Expression(SynExpr.Lambda(_)) as lambda -> 
-        match lambda with
-        | Lambda(lambda, range) -> 
-            if (not << List.isEmpty) lambda.Arguments then
-                checker args.FileContent lambda range
-            else Array.empty
-        | _ -> Array.empty
+    | AstNode.Expression(SynExpr.Lambda _) as lambda ->
+        if isForActivePatten args then
+            Array.empty
+        else
+            match lambda with
+            | Lambda(lambda, range) ->
+                if (not << List.isEmpty) lambda.Arguments then
+                    checker args.FileContent lambda range
+                else Array.empty
+            | _ -> Array.empty
     | _ -> Array.empty

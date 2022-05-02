c5ac8f631359f9138207bfa23782021092fdcba2
ijanus <emekamicro@gmail.com>
2022-05-02 12:13:29 +0100

    WIP

--- src/FSharpLint.Core/Rules/Conventions/FavourBasicControlFlow.fs
+++ src/FSharpLint.Core/Rules/Conventions/FavourBasicControlFlow.fs
@@ -6,15 +6,33 @@ open FSharp.Compiler.Syntax
 open FSharp.Compiler.Text
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
-open System
 
+let private isBasicControlFlow (synMatchClauses: List<SynMatchClause>) =
+  let isFirstNamedIdentifier firstClause =
+    match firstClause with
+    | SynMatchClause(SynPat.Named(_, _, _, _, _), _, _, _, _) -> true
+    | _ -> false
+
+  let isLastTarget lastClause =
+    match lastClause with
+    | SynMatchClause(SynPat.Wild _, None, _, _, _) -> true
+    | _ -> false
+
+  synMatchClauses.Length = 2 && isFirstNamedIdentifier synMatchClauses.[0] && isLastTarget synMatchClauses.[1]
 
 let runner args =
-    match args.AstNode with
-    | _ -> Array.empty
+  match args.AstNode with
+  | AstNode.Expression(SynExpr.Match (_, _, synMatchClauses, range)) when isBasicControlFlow synMatchClauses ->
+    { Range = range
+      Message = Resources.GetString "FavourBasicControlFlow"
+      SuggestedFix = None
+      TypeChecks = List.empty }
+    |> Array.singleton
+  | _ ->
+    Array.empty
 
 let rule =
-    { Name = "FavourBasicControlFlow"
-      Identifier = Identifiers.FavourBasicControlFlow
-      RuleConfig = { AstNodeRuleConfig.Runner = runner; Cleanup = ignore } }
-    |> AstNodeRule
+  { Name = "FavourBasicControlFlow"
+    Identifier = Identifiers.FavourBasicControlFlow
+    RuleConfig = { AstNodeRuleConfig.Runner = runner; Cleanup = ignore } }
+  |> AstNodeRule

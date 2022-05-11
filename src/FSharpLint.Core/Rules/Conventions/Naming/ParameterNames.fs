6a0ba88a916126ce2b0ba3b8993a20645f5eabbc
ijanus <emekamicro@gmail.com>
2022-05-11 11:31:10 +0100

    ParameterNames: fix some false positive
    And add unit test.

--- src/FSharpLint.Core/Rules/Conventions/Naming/ParameterNames.fs
+++ src/FSharpLint.Core/Rules/Conventions/Naming/ParameterNames.fs
@@ -20,6 +20,7 @@ let private getValueOrFunctionIdents typeChecker _accessibility pattern =
 
     match pattern with
     | SynPat.Named(_, ident, _, _, _)
+    | SynPat.LongIdent(LongIdentWithDots ([ident], _), _, _, SynArgPats.Pats [], _, _)
     | SynPat.OptionalVal(ident, _) when not (isActivePattern ident) ->
         let checkNotUnionCase = checkNotUnionCase ident
         (ident, ident.idText, Some checkNotUnionCase) |> Array.singleton
@@ -39,7 +40,7 @@ let private getIdentifiers (args:AstNodeRuleParams) =
     | AstNode.Binding(SynBinding(access, _, _, _, attributes, _, valData, pattern, _, _, _, _)) ->
         if not (isLiteral attributes) then
             match identifierTypeFromValData valData with
-            | Value | Function ->
+            | Function ->
                 let accessibility = getAccessibility args.SyntaxArray args.NodeIndex
                 getPatternIdents accessibility (getValueOrFunctionIdents args.CheckInfo) true pattern
             | Member | Property ->
--- tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/ParameterNames.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/Naming/ParameterNames.fs
@@ -68,13 +68,13 @@ let result = extractInt singleCaseDU""")
         let source = """
 module Program
 
-let __foo_bar = 0
+let someFunction __foo_bar = 0
 """
 
         let expected = """
 module Program
 
-let __foobar = 0
+let someFunction __foobar = 0
 """
 
         this.Parse source
@@ -96,3 +96,34 @@ let foo _x = 0
 
         this.Parse source
         Assert.AreEqual(expected, this.ApplyQuickFix source)
+
+let camelCaseConfig =
+    { NamingConfig.Naming = Some NamingCase.CamelCase
+      Underscores = None
+      Prefix = None
+      Suffix = None }
+
+[<TestFixture>]
+type TestConventionsCamelCase() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(ParameterNames.rule camelCaseConfig)
+
+    [<Test>]
+    member this.``PascalCase should not be flagged because FooBar is not a parameter``() =
+        this.Parse """
+type SomeType() =
+    let FooBar = 0
+    member this.SomePublicMember() =
+        ()
+"""
+
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.``PascalCase should be flagged because Bar is parameter``() =
+        this.Parse """
+module SomeModule =
+    let Foo Bar =
+        0
+"""
+
+        Assert.IsTrue this.ErrorsExist

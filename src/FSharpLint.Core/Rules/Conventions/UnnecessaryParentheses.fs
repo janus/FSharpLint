7bd6ce28df2a07cc679a4f9d83ffd8503035a980
ijanus <emekamicro@gmail.com>
2022-05-31 13:16:05 +0100

    UnnecessaryParentheses: add SuggestedFix and corresponding unit test
    Related to https://github.com/nblockchain/fantomless/commit/21ed67c5ffb514a90634541753faee254ab8d1d8

--- src/FSharpLint.Core/Rules/Conventions/UnnecessaryParentheses.fs
+++ src/FSharpLint.Core/Rules/Conventions/UnnecessaryParentheses.fs
@@ -7,15 +7,21 @@ open FSharpLint.Framework.Rules
 open FSharpLint.Framework
 open FSharpLint.Framework.Suggestion
 
+let private generateFix (text:string) range = lazy(
+    ExpressionUtilities.tryFindTextOfRange range text
+    |> Option.map (fun fromText ->
+        let toText = fromText.TrimStart('(').TrimEnd(')')
+        { FromText = fromText; FromRange = range; ToText = toText }))
+
 let private runner (args: AstNodeRuleParams) =
     match args.AstNode with
-    | AstNode.Expression(SynExpr.IfThenElse(SynExpr.Paren(expression, _, _, _), _, _, _, _, _, range)) ->
+    | AstNode.Expression(SynExpr.IfThenElse(SynExpr.Paren(expression, _, _, range), _, _, _, _, _, _)) ->
         match expression with
         | SynExpr.App(_, _, _, _, _) -> Array.empty
         | _ ->
             { Range = range
               Message = Resources.GetString("RulesUnnecessaryParenthesesError")
-              SuggestedFix = None
+              SuggestedFix = Some (generateFix args.FileContent range)
               TypeChecks = List.Empty }
             |> Array.singleton
     | _ -> Array.empty
--- tests/FSharpLint.Core.Tests/Rules/Conventions/UnnecessaryParentheses.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/UnnecessaryParentheses.fs
@@ -22,3 +22,13 @@ type TestConventionsUnnecessaryParentheses() =
     member this.``prefer conservative rule which means unnecessary parentheses is not flagged for function application``() =
         this.Parse "if (foo foobar) then bar else baz"
         Assert.IsTrue this.NoErrorsExist
+
+    [<Test>]
+    member this.``quick fix for unnecessary parentheses.``() =
+        let source = "if (foo) then bar else baz"
+
+        let expected = "if foo then bar else baz"
+
+        this.Parse source
+        Assert.AreEqual(expected, this.ApplyQuickFix source)
+

2c2ffa4c2e512e31ac0544cbc86f6334d7fa9f6d
ijanus <emekamicro@gmail.com>
2022-06-01 11:26:13 +0100

    UnnecessaryParentheses: add more unit test
    Related to https://github.com/nblockchain/fantomless/commit/cf4a4bfd80359607755bcb8aa3a5bc776532280b.

--- tests/FSharpLint.Core.Tests/Rules/Conventions/UnnecessaryParentheses.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/UnnecessaryParentheses.fs
@@ -41,3 +41,42 @@ type TestConventionsUnnecessaryParentheses() =
         this.Parse source
         Assert.AreEqual(expected, this.ApplyQuickFix source)
 
+    [<Test>]
+    member this.``parentheses around single identifiers in if expressions are unnecessary``() =
+        this.Parse """
+if (foooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo) then
+    bar else baz
+"""
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.``parentheses around single identifiers in elif expressions are unnecessary (1)``() =
+        this.Parse """
+if foo then bar
+elif (baz) then foobar
+"""
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.``parentheses shouldn't be removed (1)``() =
+        this.Parse """
+if foo then (bar)
+elif baz then foobar
+"""
+        Assert.IsTrue this.NoErrorsExist
+
+    [<Test>]
+    member this.``parentheses shouldn't be removed (2)``() =
+        this.Parse """
+if foo then bar
+elif baz then (foobar)
+"""
+        Assert.IsTrue this.NoErrorsExist
+
+    [<Test>]
+    member this.``parentheses around single identifiers in elif expressions are unnecessary (2)``() =
+        this.Parse """
+if fooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo then bar
+elif (bazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz) then foobar
+"""
+        Assert.IsTrue this.ErrorsExist

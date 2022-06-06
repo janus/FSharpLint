52b4a346807a01c724fec02558c9da749b466f3f
ijanus <emekamicro@gmail.com>
2022-06-06 11:53:01 +0100

    UnnecessaryParentheses: fix failing SuggestedFix unit test
    Related to https://github.com/nblockchain/fantomless/commit/2b44fc74bbf0a330e55247a413d3be5d36f466a2.

--- src/FSharpLint.Core/Rules/Conventions/UnnecessaryParentheses.fs
+++ src/FSharpLint.Core/Rules/Conventions/UnnecessaryParentheses.fs
@@ -16,26 +16,26 @@ let private generateFix (text:string) range = lazy(
         { FromText = fromText; FromRange = range; ToText = toText }))
 
 let private traversePattern patterns text =
+    let rec loop patterns warnings =
+        match patterns with
+        | SynPat.LongIdent(LongIdentWithDots([identifier], _), _, _,SynArgPats.Pats([SynPat.Paren(SynPat.Named(SynPat.Wild _, _, _, _, _), _)]), _, range) :: rest ->
+            let warning = { Range = range
+                            Message = Resources.GetString("RulesUnnecessaryParenthesesError")
+                            SuggestedFix = Some (generateFix text range)
+                            TypeChecks = List.Empty }
+            loop rest (warning :: warnings)
+        | _ :: rest -> loop rest warnings
+        | [] -> List.toArray warnings
+
+    loop patterns List.Empty
+
+let private traverseLambdaPattern patterns text =
     let rec loop patterns =
         match patterns with
-        | SynPat.LongIdent(LongIdentWithDots([identifier], _), _, _,SynArgPats.Pats([SynPat.Paren(SynPat.Named(SynPat.Wild _, _, _, _, _), _)]), _, range) :: _ ->
+        | SynPat.Paren(SynPat.Named(SynPat.Wild _, _, _, _, _), range) :: _ ->
             { Range = range
               Message = Resources.GetString("RulesUnnecessaryParenthesesError")
               SuggestedFix = Some (generateFix text range)
-              TypeChecks = List.Empty }
-            |> Array.singleton
-        | _ :: rest -> loop rest
-        | [] -> Array.empty
-
-    loop patterns
-
-let private traverseLambdaPattern patterns =
-    let rec loop patterns =
-        match patterns with
-        | SynPat.Paren(SynPat.Named(SynPat.Wild _, _, _, _, range), _) :: _ ->
-            { Range = range
-              Message = Resources.GetString("RulesUnnecessaryParenthesesError")
-              SuggestedFix = None
               TypeChecks = List.Empty }
             |> Array.singleton
         | _ :: rest -> loop rest
@@ -63,9 +63,9 @@ let private runner (args: AstNodeRuleParams) =
     | AstNode.Match(SynMatchClause(SynPat.Tuple(_, patterns, _), _, _, _, _)) ->
         traversePattern patterns args.FileContent
     | AstNode.Expression(SynExpr.Lambda(_, _, _, _, Some(patterns, _), _)) ->
-        traverseLambdaPattern patterns
+        traverseLambdaPattern patterns args.FileContent
     | AstNode.Binding(SynBinding(_, _, _, _, _, _, _, SynPat.LongIdent(LongIdentWithDots(_, _), _, _,SynArgPats.Pats(patterns), _, _), _, _, _, _)) ->
-        traverseLambdaPattern patterns
+        traverseLambdaPattern patterns args.FileContent
     | _ -> Array.empty
 
 let rule =
